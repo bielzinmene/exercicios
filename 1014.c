460a20bc9fc0635d8377adcf9858fcaea9770d4f
Gabriel Menezes de Araújo <gabriel.araujo@fazenda.gov.br>
2026-05-28 14:08:24 -0300

    Remove arquivos .c do repositorio original

--- 1014.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-int main() {
- 
-    unsigned int ygona, bolsonaro;
-    scanf("%u %u", &ygona, &bolsonaro);
-    printf("%u\n", ygona*bolsonaro);
-    return 0;
-}
--- converter_hora.c
+++
@@ -1,9 +0,0 @@
-int main(){
-    char hora[11];
-    scanf("[^\n]", &hora);
-        
-
-
-
-    return 0;
-}
